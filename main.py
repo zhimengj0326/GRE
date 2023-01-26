08595a980e132197c839c07ebfdd1d1e99e07554
Zirui Liu <zl105@datalab1.cs.rice.edu>
2023-01-26 14:39:23 -0600

    update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
 import torch
+import shutil
 import pdb
+import os
 import torch.nn.functional as F
 import yaml
 import editable_gnn.models as models
 from data import get_data
-from utils import prepare_dataset, compute_micro_f1, get_optimizer, set_seeds_all
+from utils import prepare_dataset, compute_micro_f1, get_optimizer, set_seeds_all, sorted_checkpoints
 from logger import Logger
 
 
@@ -19,6 +21,11 @@ parser.add_argument('--seed', default=42, type=int,
                     help='seed for initializing training. ')
 parser.add_argument('--runs', default=1, type=int,
                     help='number of runs')
+parser.add_argument('--save_total_limit', default=1, type=int,
+                    help='number of kepted models')
+
+
+ROOT = './ckpts'
 
 
 def train(model, optimizer, data, loss_op):
@@ -78,21 +85,40 @@ if __name__ == '__main__':
     print(f'whole data: {whole_data}')
 
     logger = Logger(args.runs, args)
+
+    save_path = os.path.join(ROOT, args.dataset)
+    if os.path.exists(save_path):
+        shutil.rmtree(save_path)
+    os.makedirs(save_path)
+    model_name = model_config['arch_name']
+
     for run in range(args.runs):
         set_seeds_all(args.seed + run)
         model.reset_parameters()
         optimizer = get_optimizer(model_config, model)
+        best_val = -1
+        checkpoint_prefix = f'{model_name}_run{run}'
         for epoch in range(1, model_config['epochs'] + 1):
             train(model, optimizer, train_data, loss_op)
             result = test(model, whole_data)
             logger.add_result(run, result)
             train_acc, valid_acc, test_acc = result
+            # save the model with the best valid acc
+            if valid_acc > best_val:
+                best_model_checkpoint = os.path.join(save_path, f'{checkpoint_prefix}_{epoch}.pt')
+                best_val = valid_acc
+                torch.save(model, best_model_checkpoint)
+                checkpoints_sorted = sorted_checkpoints(checkpoint_prefix, best_model_checkpoint, save_path)
+                number_of_checkpoints_to_delete = max(0, len(checkpoints_sorted) - 1)
+                checkpoints_to_be_deleted = checkpoints_sorted[:number_of_checkpoints_to_delete]
+                for checkpoint in checkpoints_to_be_deleted:
+                    os.remove(f'./{checkpoint}')
+
             print(f'Run: {run + 1:02d}, '
                     f'Epoch: {epoch:02d}, '
                     f'Train f1: {100 * train_acc:.2f}%, '
                     f'Valid f1: {100 * valid_acc:.2f}% '
                     f'Test f1: {100 * test_acc:.2f}%')
-            logger.add_result(run, result)
         logger.add_result(run, result)
         logger.print_statistics(run)
     logger.print_statistics()
--- utils.py
+++ utils.py
@@ -1,4 +1,7 @@
 import torch
+import re
+import os
+from pathlib import Path
 import time
 import numpy as np
 import random
@@ -7,6 +10,7 @@ import torch_geometric.transforms as T
 from torch_geometric.utils import subgraph
 from torch_geometric.nn.conv.gcn_conv import gcn_norm
 
+from typing import List
 
 def set_seeds_all(seed=1):
     torch.manual_seed(seed)
@@ -88,3 +92,29 @@ def compute_micro_f1(logits, y, mask=None) -> float:
             return 2 * (precision * recall) / (precision + recall)
         except ZeroDivisionError:
             return 0.
+
+
+def sorted_checkpoints(
+    checkpoint_prefix, best_model_checkpoint, output_dir=None, use_mtime=False
+) -> List[str]:
+    ordering_and_checkpoint_path = []
+    glob_checkpoints = [str(x) for x in Path(output_dir).glob(f"{checkpoint_prefix}_*")]
+
+    for path in glob_checkpoints:
+        if use_mtime:
+            ordering_and_checkpoint_path.append((os.path.getmtime(path), path))
+        else:
+            regex_match = re.match(f".*{checkpoint_prefix}_([0-9]+)", path)
+            if regex_match and regex_match.groups():
+                ordering_and_checkpoint_path.append((int(regex_match.groups()[0]), path))
+
+    checkpoints_sorted = sorted(ordering_and_checkpoint_path)
+    checkpoints_sorted = [checkpoint[1] for checkpoint in checkpoints_sorted]
+    # Make sure we don't delete the best model.
+    if best_model_checkpoint is not None:
+        best_model_index = checkpoints_sorted.index(str(Path(best_model_checkpoint)))
+        checkpoints_sorted[best_model_index], checkpoints_sorted[-1] = (
+            checkpoints_sorted[-1],
+            checkpoints_sorted[best_model_index],
+        )
+    return checkpoints_sorted
